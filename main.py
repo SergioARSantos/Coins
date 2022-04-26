f15612659c371191b5eaf7614ca9904683eed24f
Sérgio Santos <us.sasantos@formacao.ips.pt>
2022-04-26 09:07:00 +0100

    Inicio

--- main.py
+++ main.py
@@ -8,14 +8,18 @@ Image.open('popup.png').convert('RGB').save('popup.png')
 #Procurar PopUp
 def Catch_Popup():
     try:
+        #Localiza imagem no ecrã.
         position = py.locateCenterOnScreen('popup.png')
+        #Caso não encontre passa ao próximo comando.
         if position is None:
             pass
+        #Caso encontre a imagem.
         else:
             print(position)
             py.moveTo(position)
             py.click(position)
             time.sleep(3)
+            #Fecha o separador criado pelo Popup.
             py.keyDown('ctrl')
             py.press('w')
             py.keyUp('ctrl')
@@ -25,10 +29,13 @@ def Catch_Popup():
 print("Searching....")
 while True:
     count = 0
+    #Efectua uma contagem(contagem de segundos).
     while count <= 3600:
+        #Por cada segundo, efectua uma pesquisa do Popup
         Catch_Popup()
         time.sleep(1)
         count = count + 1
+        #Quando chegar a 1 hora efectua uma abertura de browser e faz 25 refresh.
         if count == 3600:
             print(count)
             try:
@@ -38,9 +45,11 @@ while True:
                 time.sleep(0.2)
                 py.press('Enter')
                 time.sleep(1)
+                #Loop para efectuar o Refresh.
                 for x in range(0, 25):
                     py.press('f5')
                     time.sleep(1)
+                #Quando termina, efectua o fecho do browser e reinicia a contagem.
                 py.keyDown('alt')
                 py.press('f4')
                 py.keyUp('alt')
